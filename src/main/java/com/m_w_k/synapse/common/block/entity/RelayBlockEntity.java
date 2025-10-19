ced978374e1c536fd4f3c95da488007610d839b0
M-W-K <31022105+M-W-K@users.noreply.github.com>
2025-10-19 16:01:47 -0600

    Relay model

--- src/main/java/com/m_w_k/synapse/common/block/entity/RelayBlockEntity.java
+++ src/main/java/com/m_w_k/synapse/common/block/entity/RelayBlockEntity.java
@@ -10,8 +10,13 @@ import com.m_w_k.synapse.common.connect.LocalAxonConnection;
 import com.m_w_k.synapse.common.connect.LocalConnectorDevice;
 import com.m_w_k.synapse.registry.SynapseBlockEntityRegistry;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.Vec3i;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -20,12 +25,72 @@ import org.jetbrains.annotations.Nullable;
  * connection length of a distributor.
  */
 public class RelayBlockEntity extends AxonBlockEntity {
+    protected static final Vec3[] SOUTH_CENTERS = new Vec3[] {
+            new Vec3(-4/16d, 4/16d, -6/16d),
+            new Vec3(4/16d, 4/16d, -6/16d),
+            new Vec3(4/16d, -4/16d, -6/16d),
+            new Vec3(-4/16d, -4/16d, -6/16d)
+    };
+    protected static final Vec3[] WEST_CENTERS = new Vec3[] {
+            new Vec3(6/16d, 4/16d, -4/16d),
+            new Vec3(6/16d, 4/16d, 4/16d),
+            new Vec3(6/16d, -4/16d, 4/16d),
+            new Vec3(6/16d, -4/16d, -4/16d)
+    };
+    protected static final Vec3[] NORTH_CENTERS = new Vec3[] {
+            new Vec3(4/16d, 4/16d, 6/16d),
+            new Vec3(-4/16d, 4/16d, 6/16d),
+            new Vec3(-4/16d, -4/16d, 6/16d),
+            new Vec3(4/16d, -4/16d, 6/16d)
+    };
+    protected static final Vec3[] EAST_CENTERS = new Vec3[] {
+            new Vec3(-6/16d, 4/16d, 4/16d),
+            new Vec3(-6/16d, 4/16d, -4/16d),
+            new Vec3(-6/16d, -4/16d, -4/16d),
+            new Vec3(-6/16d, -4/16d, 4/16d)
+    };
+    protected static final Vec3[] DOWN_CENTERS = new Vec3[] {
+            new Vec3(4/16d, 6/16d, 4/16d),
+            new Vec3(4/16d, 6/16d, -4/16d),
+            new Vec3(-4/16d, 6/16d, -4/16d),
+            new Vec3(-4/16d, 6/16d, 4/16d)
+    };
+    protected static final Vec3[] UP_CENTERS = new Vec3[] {
+            new Vec3(-4/16d, -6/16d, 4/16d),
+            new Vec3(-4/16d, -6/16d, -4/16d),
+            new Vec3(4/16d, -6/16d, -4/16d),
+            new Vec3(4/16d, -6/16d, 4/16d)
+    };
 
     public RelayBlockEntity(BlockPos pos, BlockState state) {
         super(SynapseBlockEntityRegistry.RELAY_BLOCK.get(), pos, state);
         for (var pair : AxonDeviceDefinitions.RELAYS_INV) {
             devices.add(new LocalConnectorDevice(pair.value(), ConnectorLevel.RELAY));
         }
+    }
+
+    @Override
+    public @NotNull Vec3 renderOffsetForSlot(int slot, AxonBlockEntity other) {
+        Direction dir = getBlockState().getValue(RelayBlock.MOUNT_DIRECTION);
+
+        return centers(dir)[AxonDeviceDefinitions.RELAYS_INV.get(slot).firstInt()];
+    }
+
+    public static Vec3[] centers(Direction mount) {
+        return switch (mount) {
+            case DOWN -> DOWN_CENTERS;
+            case UP -> UP_CENTERS;
+            case NORTH -> NORTH_CENTERS;
+            case SOUTH -> SOUTH_CENTERS;
+            case WEST -> WEST_CENTERS;
+            case EAST -> EAST_CENTERS;
+        };
+    }
+
+    @Override
+    public @NotNull Vec3 renderDirectionForSlot(int slot, AxonBlockEntity other) {
+        Vec3i norm = getBlockState().getValue(RelayBlock.MOUNT_DIRECTION).getNormal();
+        return new Vec3(norm.getX(), norm.getY(), norm.getZ());
     }
 
     @Override
--- src/main/java/com/m_w_k/synapse/data/SynapseBlockStateProvider.java
+++ src/main/java/com/m_w_k/synapse/data/SynapseBlockStateProvider.java
@@ -2,12 +2,15 @@ package com.m_w_k.synapse.data;
 
 import com.m_w_k.synapse.SynapseMod;
 import com.m_w_k.synapse.common.block.EndpointBlock;
+import com.m_w_k.synapse.common.block.RelayBlock;
 import com.m_w_k.synapse.registry.SynapseBlockRegistry;
+import net.minecraft.core.Direction;
 import net.minecraft.data.PackOutput;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.client.model.generators.ConfiguredModel;
 import net.minecraftforge.client.model.generators.ModelFile;
+import net.minecraftforge.client.model.generators.MultiPartBlockStateBuilder;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 public class SynapseBlockStateProvider extends BlockStateProvider {
@@ -28,7 +31,34 @@ public class SynapseBlockStateProvider extends BlockStateProvider {
                 .part().modelFile(endpoint).rotationY(270).addModel().condition(EndpointBlock.WEST, true).end()
                 .part().modelFile(endpoint).rotationX(90).addModel().condition(EndpointBlock.DOWN, true).end()
                 .part().modelFile(endpoint).rotationX(-90).addModel().condition(EndpointBlock.UP, true).end();
+
         simpleBlockItem(SynapseBlockRegistry.ENDPOINT_BASIC.get(), endpoint);
+        var builder = getMultipartBuilder(SynapseBlockRegistry.RELAY.get());
+        for (Direction dir : Direction.values()) {
+            relaySector(builder, dir);
+        }
+        simpleBlockItem(SynapseBlockRegistry.RELAY.get(), models().getExistingFile(modLoc("block/relay_1")));
+    }
+
+    private void relaySector(MultiPartBlockStateBuilder relayBuilder, Direction sector) {
+        int x = 0;
+        if (sector.getAxis().isHorizontal()) {
+            x = -90;
+        } else if (sector == Direction.DOWN) {
+            x = 180;
+        }
+        relayBuilder.part().modelFile(models().getExistingFile(modLoc("block/relay_1")))
+                .rotationX(x).rotationY((int) sector.toYRot()).addModel()
+                .condition(RelayBlock.MOUNT_DIRECTION, sector).condition(RelayBlock.RELAYS, 1, 2, 3, 4).end();
+        relayBuilder.part().modelFile(models().getExistingFile(modLoc("block/relay_2")))
+                .rotationX(x).rotationY((int) sector.toYRot()).addModel()
+                .condition(RelayBlock.MOUNT_DIRECTION, sector).condition(RelayBlock.RELAYS, 2, 3, 4).end();
+        relayBuilder.part().modelFile(models().getExistingFile(modLoc("block/relay_3")))
+                .rotationX(x).rotationY((int) sector.toYRot()).addModel()
+                .condition(RelayBlock.MOUNT_DIRECTION, sector).condition(RelayBlock.RELAYS, 3, 4).end();
+        relayBuilder.part().modelFile(models().getExistingFile(modLoc("block/relay_4")))
+                .rotationX(x).rotationY((int) sector.toYRot()).addModel()
+                .condition(RelayBlock.MOUNT_DIRECTION, sector).condition(RelayBlock.RELAYS, 4).end();
     }
 
     private void simple(Block block, String file) {
